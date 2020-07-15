56175a129d15e3e163fe299e55ba3705b904c885
Michelle Chen <mich.chen.94@gmail.com>
2020-07-15 20:19:19 +0000

    modified program to allow n number of words to use as n-gram.

--- markov.py
+++ markov.py
@@ -16,7 +16,7 @@ def open_and_read_file(file_path):
 # print(open_and_read_file("green-eggs.txt"))
 
 
-def make_chains(text_string):
+def make_chains(text_string, n):
     """Take input text as string; return dictionary of Markov chains.
 
     A chain will be a key that consists of a tuple of (word1, word2)
@@ -46,50 +46,63 @@ def make_chains(text_string):
     # print(words, '\n')
 
     # from 0 to len(words) - 2 to allow us to access index + 2
-    for index in range(0, len(words) - 2):
+    for index in range(0, len(words) - n):
         # set key as a tuple of bigrams
-        key = (words[index], words[index + 1])
+        key = []
+        words_index = index
+        for i in range(n):
+            key.append(words[i + words_index])
+        key = tuple(key)
+
         if key not in chains:
             # check if key exists in dictionary
             # if not then create new key and set value to following word
-            chains[key] = [words[index + 2]]
+            chains[key] = [words[index + n]]
             
         else:
             # if key does exist in dictionary already
             # then append new following word into existing list of values
-            chains[key].append(words[index + 2])
-        
-    return chains
+            chains[key].append(words[index + n])
+
        
     # for key, value in chains.items():
     #     print(key, ':', value)
-    # # print(chains)
+    # print(chains)
+    return chains
 
 
-# ('Would', 'you') : ['could', 'could', 'could', 'could', 'like', 'like']
-# ('you', 'could') : ['you', 'you', 'you', 'you']
-# ('could', 'you') : ['in', 'with', 'in', 'with']
-# ('you', 'in') : ['a', 'a']
-# ('in', 'a') : ['house?', 'box?']
-# ('a', 'house?') : ['Would']
-# ('house?', 'Would') : ['you']
-# ('you', 'with') : ['a', 'a']
-# ('with', 'a') : ['mouse?', 'fox?']
-# ('a', 'mouse?') : ['Would']
-# ('mouse?', 'Would') : ['you']
-# ('a', 'box?') : ['Would']
-# ('box?', 'Would') : ['you']
-# ('a', 'fox?') : ['Would']
-# ('fox?', 'Would') : ['you']
-# ('you', 'like') : ['green', 'them,']
-# ('like', 'green') : ['eggs']
-# ('green', 'eggs') : ['and']
-# ('eggs', 'and') : ['ham?']
-# ('and', 'ham?') : ['Would']
-# ('ham?', 'Would') : ['you']
-# ('like', 'them,') : ['Sam']
-# ('them,', 'Sam') : ['I']
-# ('Sam', 'I') : ['am?']
+
+# ('Would', 'you', 'could', 'you') : ['in', 'with', 'in', 'with']
+# ('you', 'could', 'you', 'in') : ['a', 'a']
+# ('could', 'you', 'in', 'a') : ['house?', 'box?']
+# ('you', 'in', 'a', 'house?') : ['Would']
+# ('in', 'a', 'house?', 'Would') : ['you']
+# ('a', 'house?', 'Would', 'you') : ['could']
+# ('house?', 'Would', 'you', 'could') : ['you']
+# ('you', 'could', 'you', 'with') : ['a', 'a']
+# ('could', 'you', 'with', 'a') : ['mouse?', 'fox?']
+# ('you', 'with', 'a', 'mouse?') : ['Would']
+# ('with', 'a', 'mouse?', 'Would') : ['you']
+# ('a', 'mouse?', 'Would', 'you') : ['could']
+# ('mouse?', 'Would', 'you', 'could') : ['you']
+# ('you', 'in', 'a', 'box?') : ['Would']
+# ('in', 'a', 'box?', 'Would') : ['you']
+# ('a', 'box?', 'Would', 'you') : ['could']
+# ('box?', 'Would', 'you', 'could') : ['you']
+# ('you', 'with', 'a', 'fox?') : ['Would']
+# ('with', 'a', 'fox?', 'Would') : ['you']
+# ('a', 'fox?', 'Would', 'you') : ['like']
+# ('fox?', 'Would', 'you', 'like') : ['green']
+# ('Would', 'you', 'like', 'green') : ['eggs']
+# ('you', 'like', 'green', 'eggs') : ['and']
+# ('like', 'green', 'eggs', 'and') : ['ham?']
+# ('green', 'eggs', 'and', 'ham?') : ['Would']
+# ('eggs', 'and', 'ham?', 'Would') : ['you']
+# ('and', 'ham?', 'Would', 'you') : ['like']
+# ('ham?', 'Would', 'you', 'like') : ['them,']
+# ('Would', 'you', 'like', 'them,') : ['Sam']
+# ('you', 'like', 'them,', 'Sam') : ['I']
+# ('like', 'them,', 'Sam', 'I') : ['am?']
 
 
 def make_text(chains):
@@ -98,19 +111,21 @@ def make_text(chains):
     words = []
 
     list_keys = list(chains.keys())
+    # list of tuple keys
     key1 = random.choice(list_keys)
     value1 = random.choice(chains[key1])
-    link1 = f'{key1[0]} {key1[1]} {value1}'
+    link1 = f'{" ".join(key1)} {value1}'
     words.append(link1)
 
     while key1 in chains:
-        new_key = link1.split(" ")[-2:]
+        new_key = link1.split(" ")[1:]
         new_key_tuple = tuple(new_key)
+
         if new_key_tuple in chains.keys():
             new_value = random.choice(chains[new_key_tuple])
             words.append(new_value)
             key1 = new_key_tuple
-            link1 = f'{new_key[0]} {new_key[1]} {new_value}'
+            link1 = f'{" ".join(new_key_tuple)} {new_value}'
         else:
             break
     
@@ -122,7 +137,7 @@ input_path = sys.argv[1]
 input_text = open_and_read_file(input_path)
 
 # Get a Markov chain
-chains = make_chains(input_text)
+chains = make_chains(input_text, 4)
 
 # Produce random text
 random_text = make_text(chains)
